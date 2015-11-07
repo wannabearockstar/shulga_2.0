74233fe39fd96617635d1afaa4044d3563bf5e4b
wannabe <smelov.kirill@gmail.com>
2015-11-07 22:00:04 +1000

    Demo and index page placeholders

--- src/main/java/ga/model/schedule/time/WeekDay.java
+++ src/main/java/ga/model/schedule/time/WeekDay.java
@@ -6,9 +6,8 @@ import com.fasterxml.jackson.annotation.JsonValue;
 import java.util.HashMap;
 import java.util.Map;
 
-public enum  WeekDay {
+public enum WeekDay {
 
-    UNKNOWN(0),
 
     MONDAY(1),
 
@@ -20,9 +19,7 @@ public enum  WeekDay {
 
     FRIDAY(5),
 
-    SATURDAY(6),
-
-    SUNDAY(7);
+    SATURDAY(6);
 
     public static final Map<WeekDay, String> locale;
     private static final Map<Integer, WeekDay> LOOKUP = new HashMap<>(values().length);
@@ -33,14 +30,12 @@ public enum  WeekDay {
         }
 
         locale = new HashMap<>();
-        locale.put(UNKNOWN, "Неизвестно");
         locale.put(MONDAY, "Понедельник");
         locale.put(TUESDAY, "Вторник");
         locale.put(WEDNESDAY, "Среда");
         locale.put(THURSDAY, "Четверг");
         locale.put(FRIDAY, "Пятница");
         locale.put(SATURDAY, "Суббота");
-        locale.put(SUNDAY, "Воскресенье");
     }
 
     private int day;
@@ -52,7 +47,7 @@ public enum  WeekDay {
     @JsonCreator
     public static WeekDay get(int day) {
         if (!LOOKUP.containsKey(day)) {
-            return UNKNOWN;
+            return MONDAY;
         }
 
         return LOOKUP.get(day);
@@ -64,6 +59,6 @@ public enum  WeekDay {
     }
 
     public boolean isWeekend() {
-        return this == SATURDAY || this == SUNDAY;
+        return this == SATURDAY;
     }
 }
---
+++ src/main/java/web/controller/IndexController.java
@@ -0,0 +1,16 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by wannabe on 07.11.15.
+ */
+@Controller
+public class IndexController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String indexPage() {
+        return "index";
+    }
+}

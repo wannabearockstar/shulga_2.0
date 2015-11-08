056862d335a504264a1c24130f23e88e3470834b
wannabe <smelov.kirill@gmail.com>
2015-11-08 10:40:22 +1000

    Add csv export

--- src/main/java/web/controller/OutController.java
+++ src/main/java/web/controller/OutController.java
@@ -1,5 +1,7 @@
 package web.controller;
 
+import mapper.ScheduleConfigLoader;
+import mapper.serializer.ScheduleCsvSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,9 +10,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import web.model.Result;
 import web.service.data.DataService;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Controller
@@ -29,5 +34,19 @@ public class OutController {
     @RequestMapping(value = "/{id}/data", method = RequestMethod.GET)
     public ResponseEntity<Result> getResult(@PathVariable("id") int id) {
         return new ResponseEntity<>(Result.success(dataService.getResult(id)), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}/download", method = RequestMethod.GET)
+    public ModelAndView downloadResult(@PathVariable("id") int id, HttpServletResponse response) throws IOException {
+        response.setContentType("applications/csv");
+        response.setContentType("applications/unknown");
+//        response.setCharacterEncoding("UTF-16LE");
+        response.setHeader("content-disposition", "attachment;filename =data_" + id + ".csv");
+
+        ServletOutputStream writer = response.getOutputStream();
+        writer.write(new ScheduleCsvSerializer().serialize(ScheduleConfigLoader.fromLocalSchedule(String.format("schedule_result_%d.json", id))).getBytes("UTF-8"));
+        writer.flush();
+        writer.close();
+        return null;
     }
 }

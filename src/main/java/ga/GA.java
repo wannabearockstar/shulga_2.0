f4e137e4cd7b2eab26106cc3ca0b3437399551ff
wannabe <smelov.kirill@gmail.com>
2015-11-08 20:10:40 +1000

    fix

--- src/main/java/ga/GA.java
+++ src/main/java/ga/GA.java
@@ -10,7 +10,7 @@ import ga.model.config.ScheduleConfig;
 public class GA {
 
     public static int solve(ScheduleConfig scheduleConfig, int id) {
-        AlgorithmConfig algConfig = new AlgorithmConfig(10000, 60);
+        AlgorithmConfig algConfig = new AlgorithmConfig(5000, 50);
         FitnessHandler handler = new FitnessHandlerImpl();
         Algorithm algorithm = new AlgorithmImpl(algConfig, scheduleConfig, handler, id);
         new Thread(algorithm).start();
